d14a04e690a52ea7cdd46ffbb3e8144ff6421ec4
wuxiaopeng <1099272020@qq.com>
2018-09-14 14:06:49 +0800

    修改项目

--- src/main/java/com/example/eureka/controller/swaggerController/AnimalError.java
+++
@@ -1,9 +0,0 @@
-package com.example.eureka.controller.swaggerController;
-
-/**
- * @author wuxiaopeng
- * @create 2018-07-12 17:56
- **/
-
-public class AnimalError {
-}   
---
+++ src/main/java/com/example/springBootDemo/EurekaServer.java
@@ -0,0 +1,20 @@
+package com.example.springBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册服务中心：心跳检查，客户端的缓存。提高可用性和伸缩。
+ * wxp
+ * 2018/8/31
+ */
+//启动一个服务注册中心提供给其他应用进行对话
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer.class, args);
+	}
+}
